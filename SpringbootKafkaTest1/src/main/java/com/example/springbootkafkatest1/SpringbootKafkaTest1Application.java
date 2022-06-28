91f5aa62d90c3f7c0c55f0d233e1a6d0f528828f
Dion Kadriu <dik00028@students.stir.ac.uk>
2022-06-28 23:09:47 +0100

    Initial commit

---
+++ SpringbootKafkaTest1/src/main/java/com/example/springbootkafkatest1/SpringbootKafkaTest1Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkatest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaTest1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootKafkaTest1Application.class, args);
+  }
+
+}
